f9bc00db61d1defcbc26e026a91df349763e69e6
kangna <kangna@moe.com>
2018-04-17 14:54:07 +0800

    登录模块完成

--- simulation/login.js
+++ simulation/login.js
@@ -1,35 +1,36 @@
 const axios = require('axios'),
     querystring = require('querystring'),
-    qs = require('qs'),
-    config = require('../config/loginConfig')
-
-const hed = {
-    'Referer': 'https://accounts.pixiv.net/login?lang=zh&source=pc&view_type=page&ref=wwwtop_accounts_index',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
-}
+    config = require('../config/config'),
+    ker = require('./getKey')
 
 const reqpixiv = {
-    loging: async (key) => {
+    loging: async (key, cookie) => {
         try {
-            console.log(key)
-            let {data, status, headers, config} = await axios.post('https://accounts.pixiv.net/api/login?lang=zh', querystring.stringify({
-                pixiv_id: 'mikulength',
-                captcha: '',
-                g_recaptcha_response: '',
-                password: 'yingjie1233210',
-                post_key: key,
-                source: 'pc',
-                ref: 'wwwtop_accounts_index',
-                return_to: 'https://www.pixiv.net/',
-            }), {headers:hed})
-            if (status === 200) {
-                console.log(data)
+            let {data, status} = await axios({
+                method: 'post',
+                url: config.url.login_api,
+                headers: config.url.post_headers,
+                data: querystring.stringify({
+                    pixiv_id: 'mikulength',
+                    password: 'yingjie1233210',
+                    captcha: '',
+                    g_recaptcha_response: '',
+                    post_key: await ker.getKey.logingkey(),
+                    source: 'pc',
+                    ref: 'wwwtop_accounts_index',
+                    return_to: 'http://www.pixiv.net/'
+                })
+            })
+            if (status === 200 && data.body.success) {
+                console.log('login success')
             }
         } catch (e) {
             console.log(e)
         }
     }
 }
+
+reqpixiv.loging()
 
 module.exports = {
     reqpixiv
