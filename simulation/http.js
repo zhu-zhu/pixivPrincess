81c90705b82edae66c656229fd28ea4b4409646d
kangna <kangna@moe.com>
2018-04-20 13:10:46 +0800

    commit

--- simulation/http.js
+++ simulation/http.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
-    config = require('../config/config')
+    config = require('../config/config'),
+        fs = require("fs")
 
 const url = 'https://www.pixiv.net/ranking.php?mode=daily'
 
@@ -9,12 +10,24 @@ const http = {
             let { data, status } = await axios({
                 method: 'get',
                 url: url,
-                headers: {
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
-                    'Cookie': config.url.cookieer
-                }
+                // headers: {
+                //     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+                //     'Cookie': config.url.cookieer
+                // }
             })
             if (status === 200) {
+                console.log("准备写入文件");
+                fs.writeFile('input.html', data,  function(err) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    console.log("数据写入成功！");
+                    fs.readFile('input.html', function (err, data) {
+                        if (err) {
+                            return console.error(err);
+                        }
+                    });
+                });
                 return data
             }
         } catch (e) {
